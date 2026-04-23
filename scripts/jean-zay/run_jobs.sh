df56271e6861ad9bc72d1198e5c7a439c53bd22b
ShadowDragon5 <laurynas.varnas@proton.me>
2026-04-23 13:38:14 +0200

    Fix slurm scripts

--- scripts/jean-zay/run_jobs.sh
+++ scripts/jean-zay/run_jobs.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-OUT_PREF="${SCRATCH}/out"
+OUT_PREF="$SCRATCH/out/"
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
 
 run_job() {
 	NAME="$1"
 	shift
-	sbatch --job-name="GNN_${NAME}" --error="${OUT_PREF}err_${NAME}" --output="${OUT_PREF}out_${NAME}" \
-		job.sh q=true dev=jean_zay "$@"
+	sbatch --job-name="${NAME}" --error="${OUT_PREF}err_${NAME}" --output="${OUT_PREF}out_${NAME}" \
+		"$SCRIPT_PATH/job.slurm" dev=jean_zay "$@"
 }
 
 run_job "CIF2T1" full_batches=1 --config-name cifar10_Adag
 run_job "CIF2Tall" full_batches=all --config-name cifar10_Adag
+
+run_job "C2p10Tall" pre_epochs=10 full_batches=all --config-name cifar10_Adag
+run_job "C2p10T1" pre_epochs=10 full_batches=1 --config-name cifar10_Adag
+run_job "C2p10T10" pre_epochs=10 full_batches=10 --config-name cifar10_Adag
